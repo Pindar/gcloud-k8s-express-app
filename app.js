dd54466eccf5e684a1078b53611b46d916d652d0
Simon Dittlmann <simon@itnotes.de>
2017-02-03 17:10:29 +0100

    remove morgan as logger

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -16,9 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
 // Add the request logger before anything else so that it can
 // accurately log requests.
 // [START requests]
