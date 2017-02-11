2f77cc68743b09b05e65fd7643ea350815d93cfe
Simon Dittlmann <simon@itnotes.de>
2017-02-11 17:13:01 +0100

    fix image replacement

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -15,7 +15,7 @@ sed -i.bak 's#PRODUCTION_DOMAIN#$PROD_SUBDOMAIN.$DOMAIN#' k8s/ingress/*.yaml
 if [[ "$CI_ENVIRONMENT" = "production" || "$CI_ENVIRONMENT" = "staging" ]]; then
   create_namespace production
   # update docker image to latest
-  sed -i.bak 's#IMAGE_PLACEHOLDER#$CI_REGISTRY_IMAGE:${CI_BUILD_TAG:-`echo $CI_BUILD_REF | head -c 8`}#' k8s/${CI_ENVIRONMENT}/*.yaml
+  sed -i.bak "s#IMAGE_PLACEHOLDER#$CI_REGISTRY_IMAGE:${CI_BUILD_TAG:-`echo $CI_BUILD_REF | head -c 8`}#" k8s/${CI_ENVIRONMENT}/*.yaml
   # apply changes and create/update ingress
   kubectl apply --namespace=production -f k8s/${CI_ENVIRONMENT}/ --record
   kubectl apply --namespace=production -f k8s/ingress/ --record
@@ -23,7 +23,7 @@ if [[ "$CI_ENVIRONMENT" = "production" || "$CI_ENVIRONMENT" = "staging" ]]; then
 else
   create_namespace ${CI_ENVIRONMENT}
   # update docker image to latest
-  sed -i.bak 's#IMAGE_PLACEHOLDER#$CI_REGISTRY_IMAGE:${CI_BUILD_TAG:-`echo $CI_BUILD_REF | head -c 8`}#' k8s/dev/*.yaml
+  sed -i.bak "s#IMAGE_PLACEHOLDER#$CI_REGISTRY_IMAGE:${CI_BUILD_TAG:-`echo $CI_BUILD_REF | head -c 8`}#" k8s/dev/*.yaml
   # apply changes
   kubectl apply --namespace=${CI_ENVIRONMENT} -f k8s/dev/ --record
 fi
