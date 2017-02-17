922c011f90ae283b45fa75566866907159bb303d
Simon Dittlmann <simon@itnotes.de>
2017-02-17 22:53:54 +0100

    fix teardown script

--- k8s/teardown.sh
+++ k8s/teardown.sh
@@ -2,5 +2,5 @@
 
 # delete ingress explicitly because otherwise you will have left over resources in your google cloud platform
 # e.g., load balancer, health check etc. won't be removed
-kubectl delete ing k8s/ingress/ingress.yaml --namespace production
+kubectl delete ing -f k8s/ingress/ingress.yaml --namespace production
 kubectl delete ns production
