5f0161c2e3d8acf270d617ec403384ce256faf1b
Simon Dittlmann <simon@itnotes.de>
2017-05-02 22:47:37 +0200

    fix deployment manager files

--- gcp/setup_gcloud_project.sh
+++ gcp/setup_gcloud_project.sh
@@ -2,4 +2,5 @@
 
 # https://cloudplatform.googleblog.com/2017/04/automating-project-creation-with-Google-Cloud-Deployment-Manager.html?m=1
 # gcloud deployment-manager deployments create <newproject_deployment> --config config.yaml --project <Project Creation project>
+# $PROJECT_DEPLOYMENT is between 4 and 30 characters
 gcloud deployment-manager deployments create $PROJECT_DEPLOYMENT --config config.yaml --project $PROJECT_CREATION_PROJECT
